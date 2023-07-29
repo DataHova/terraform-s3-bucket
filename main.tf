fd4eff727a89306a59934945212c88d7789b1fa7
sillians <ihuomacbasil@gmail.com>
2023-07-29 11:15:29 +0100

    Changes on versioning

--- main.tf
+++ main.tf
@@ -1,12 +1,27 @@
-resource "aws_s3_bucket" "storage" {
-    bucket = var.bucket_name
-
-    tags = {
-        Name = var.tag_name
-        Environment = var.env_name
-    }
+provider "aws" {
+  region = var.region
 }
 
+resource "aws_s3_bucket" "storage" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.tag_name
+    Environment = var.env_name
+  }
+}
+
+# resource "aws_s3_bucket_acl" "storage" {
+#   bucket = aws_s3_bucket.storage.id
+#   acl    = var.acl
+# }
+
+resource "aws_s3_bucket_versioning" "storage" {
+  bucket = aws_s3_bucket.storage.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 resource "aws_s3_bucket_public_access_block" "storage" {
   bucket = aws_s3_bucket.storage.id
--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,24 @@
 output "bucket" {
-  value       = aws_s3_bucket.storage
+  value = aws_s3_bucket.storage
 }
 
-output "bucket_name" {
+output "s3_bucket_bucket_domain_name" {
   description = "Name of the created bucket"
   value       = aws_s3_bucket.storage.bucket
 }
 
-# output "bucket_arn" {
-#   value       = aws_s3_bucket.storage.arn
-# }
-
-output "name" {
-  value       = aws_s3_bucket.storage.id
+output "s3_bucket_id" {
+  value = aws_s3_bucket.storage.id
 }
 
 
+output "bucket_s3_bucket_arnarn" {
+  description = "The ARN of the bucket"
+  value       = aws_s3_bucket.storage.arn
+}
+
+
+output "s3_bucket_region" {
+  description = "The AWS region this bucket resides in."
+  value       = aws_s3_bucket.storage.region
+}
